c238b3c3584e3e5f65f35cca3687e79b468e5442
RTC CODES <150908947+Bonzskie01@users.noreply.github.com>
2024-10-15 10:45:06 +0800

    Update and rename 04TextFileManipulationBufferedReader.java to TextFileManipulationBufferedReader.java

--- Gclass/Text File Manipulation - Java/TextFileManipulationBufferedReader.java
+++ Gclass/Text File Manipulation - Java/TextFileManipulationBufferedReader.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class BufferedReaderTextFile {
+public class TextFileManipulationBufferedReader {
     public static void main(String[] args) throws Exception {
 		File myFile = new File("MyFile.txt");
         BufferedReader br = new BufferedReader(new FileReader(myFile));
