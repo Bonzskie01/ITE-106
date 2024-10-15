f1cbbdbd185c75a6e658b5303b1d517e7181ade7
RTC CODES <150908947+Bonzskie01@users.noreply.github.com>
2024-10-15 10:46:14 +0800

    Update and rename 05TextFileManipulationFileReader.java to TextFileManipulationFileReader.java

--- Gclass/Text File Manipulation - Java/TextFileManipulationFileReader.java
+++ Gclass/Text File Manipulation - Java/TextFileManipulationFileReader.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileReader;
 
-public class FileReaderTextFile {
+public class TextFileManipulationFileReader {
     public static void main(String[] args) throws Exception {
 		File myFile = new File("MyFile.txt");
         FileReader fr = new FileReader(myFile);
