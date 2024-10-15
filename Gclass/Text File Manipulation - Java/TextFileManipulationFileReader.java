93ed8162ab6516af68b8578f995b9493281873e3
RTC CODES <150908947+Bonzskie01@users.noreply.github.com>
2024-10-15 10:46:27 +0800

    Update TextFileManipulationFileReader.java

--- Gclass/Text File Manipulation - Java/TextFileManipulationFileReader.java
+++ Gclass/Text File Manipulation - Java/TextFileManipulationFileReader.java
@@ -3,7 +3,7 @@ import java.io.FileReader;
 
 public class TextFileManipulationFileReader {
     public static void main(String[] args) throws Exception {
-		File myFile = new File("MyFile.txt");
+	File myFile = new File("MyFile.txt");
         FileReader fr = new FileReader(myFile);
         int i;
         while((i = fr.read()) != -1) {
